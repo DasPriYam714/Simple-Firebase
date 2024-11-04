4f19650e9902945b7d7632f4367e094f8f36545b
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-11-04 19:05:32 +0600

    add react router dom

---
+++ src/assets/__steps.js
@@ -0,0 +1,9 @@
+/*
+*1.visit: console.firebase.google.com
+*2.create firebase project.[skip google analytics]
+*3.register app[create config]
+*4. install firebase
+*5. add config file to the project 
+*6. do not publish firebase config file to push in a public repository
+7.
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDukJ7wXFsFvKFwAZ-XIfOHCWzSGJ-jJfs",
+  authDomain: "simple-firebase-5075d.firebaseapp.com",
+  projectId: "simple-firebase-5075d",
+  storageBucket: "simple-firebase-5075d.firebasestorage.app",
+  messagingSenderId: "403641320086",
+  appId: "1:403641320086:web:2eb428860094c62036b8b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
