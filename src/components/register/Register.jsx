3ac925d668455a4336723f173a4908f41bc63d88
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-11-21 11:32:29 +0600

    add login with  email and  password

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import app from '../../firebase/firebase.init';
+import { createUserWithEmailAndPassword,getAuth } from 'firebase/auth';
 
 const Register = () => {
+    
     const handleRegister= e =>{
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
+
+        const auth= getAuth(app);
+
+    createUserWithEmailAndPassword(auth,email,password)
+    .then(result=>{
+        const user=result.user;
+    })
+    .catch(error=>{
+        console.log(error);
+    });
     }
+    
     return (
         <div className='flex flex-col justify-center text-center'>
             <h2>Register your account</h2>
